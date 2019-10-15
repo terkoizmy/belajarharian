51fbc834d8f092fba7e5e1caa0b6b08c4c032b6d
unknown <dimas.aryan021@gmail.com>
2019-10-15 09:37:32 +0700

    week 2 execises 4

---
+++ switch.js
@@ -0,0 +1,22 @@
+
+var tanggal = 11
+var bulan = 5
+var tahun = 1990
+
+switch(bulan) {
+    case 1:   { console.log(tanggal + ' ' + 'januari' + ' ' + tahun); break; }
+    case 2:   { console.log(tanggal + ' ' + 'febuari' + ' ' + tahun); break; }
+    case 3:   { console.log(tanggal + ' ' + 'maret' + ' ' + tahun); break; }
+    case 4:   { console.log(tanggal + ' ' + 'april' + ' ' + tahun); break; }
+    case 5:   { console.log(tanggal + ' ' + 'mei' + ' ' + tahun); break; }
+    case 6:   { console.log(tanggal + ' ' + 'juni' + ' ' + tahun); break; }
+    case 7:   { console.log(tanggal + ' ' + 'juli' + ' ' + tahun); break; }
+    case 8:   { console.log(tanggal + ' ' + 'agustus' + ' ' + tahun); break; }
+    case 9:   { console.log(tanggal + ' ' + 'september' + ' ' + tahun); break; }
+    case 10:  { console.log(tanggal + ' ' + 'oktober' + ' ' + tahun); break; }
+    case 11:  { console.log(tanggal + ' ' + 'november' + ' ' + tahun); break; }
+    case 12:  { console.log(tanggal + ' ' + 'desember' + ' ' + tahun); break; }
+  }
+  
+
+
